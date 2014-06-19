9be0a1e3de6380ee67d305f9367994746a1f8b4f
sigibart <sygiuz@gmail.com>
2014-06-19 23:35:02 +0300

    Second commit

---
+++ sort_string.rb
@@ -0,0 +1,11 @@
+=begin
+Problem Statement
+Create a method named 'sort_string' which accepts a String and rearranges 
+all the words in ascending order, by length. Let's not treat the punctuation
+marks any different than other characters and assume that we will always have
+single space to separate the words. 
+=end
+
+def sort_string
+
+end
